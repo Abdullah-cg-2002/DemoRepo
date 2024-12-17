ade05a6537795215131f0fca37d8fa7ad3933a2b
Sarfudeen <mohammed-zaman-abdullah-kasali.sarfudeen@capgemini.com>
2024-12-17 16:30:03 +0530

    Commit Array file

---
+++ Array.java
@@ -0,0 +1,45 @@
+package Oct18;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[] = {1,2,3,4,5,6,7,8,9};
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			int temp = arr[i];
+			if(i%2 == 0)
+			{
+				int max = i;
+				for(int j=i+1;j<arr.length;j++)
+				{
+					if(arr[max] < arr[j])
+						max = j;
+				}
+				
+				arr[i] = arr[max];
+				arr[max] = temp;
+			}
+			
+			else
+			{
+				int min = i;
+				for(int j=i+1;j<arr.length;j++)
+				{
+					if(arr[min] > arr[j])
+						min = j;
+				}
+				
+				arr[i] = arr[min];
+				arr[min] = temp;
+			}
+		}
+		
+		for(int i : arr)
+			System.out.print(i+" ");
+
+	}
+
+}
